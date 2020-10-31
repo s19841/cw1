76f6029e63b31c92f583b0726a7473454c6c49d5
Piotr Flis <s19841@pjwstk.edu.pl>
2020-10-31 22:38:44 +0100

    Commit 3 - Zadanie 4

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,9 +11,16 @@ namespace Cw1
     {
        public static async Task Main(string[] args)
        {
-           string url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+           if (args.Length == 0) throw new ArgumentException("Brak adresu URL");
+
+           var urlRegex = new Regex("^(https?://)?[0 9a-zA-Z].[-_0-9a-zA-Z].[0-9a-zA-Z]+$");
+
+            string url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+           if (urlRegex.Matches(url).Count == 0) throw new ArgumentException();
+
             var httpClient = new HttpClient();
             var response = await httpClient.GetAsync(url);
+            var zbior = new HashSet<string>();
 
             if (response.IsSuccessStatusCode)
             {
@@ -19,12 +28,32 @@ namespace Cw1
                 var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
 
                 MatchCollection matches = regex.Matches(html);
-                foreach (var i in matches)
+                if (matches.Count == 0)
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine("Nie znaleziono adresów email");
                 }
-                
+                else
+                {
+                    foreach (var i in matches)
+                    {
+                        zbior.Add(i.ToString());
+                        
+                    }
+
+                    var zbior2 = zbior.Distinct();
+                    foreach (var i in zbior2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
             }
+            else
+            {
+                Console.WriteLine("Błąd wczasie pobierania strony");
+                return;
+            }
+            httpClient.Dispose();
         }
     }
 }
